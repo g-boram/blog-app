20839006b1ee15c3abb7a87959ea169c5c828363
GOBORAM <109337202+g-boram@users.noreply.github.com>
2023-12-28 15:02:00 +0900

    #3-로그인 여부에 따른 Route 분기처리

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -7,21 +7,37 @@ import PostEdit from '../pages/posts/edit';
 import ProfilePage from '../pages/profile';
 import LoginPage from '../pages/login';
 import SignupPage from '../pages/signup';
+import { useState } from 'react';
 
 export default function Router() {
+
+  // firebase Auth가 인증되었으면 true로 변경해주는 로직 추가
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/posts' element={<PostList />}/>
-        <Route path='/posts/:id' element={<PostDetail />}/>
-        <Route path='/posts/new' element={<PostNew />}/>
-        <Route path='/posts/edit/:id' element={<PostEdit />}/>
-        <Route path='/profile' element={<ProfilePage />}/>
-        <Route path='/login' element={<LoginPage />}/>
-        <Route path='/signup' element={<SignupPage />}/>
-        {/* 잘못된 경로 진입시 돌아갈 경로 설정 */}
-        <Route path='*' element={<Navigate replace to="/" />} />
+        {isAuthenticated ? (
+          <>
+            <Route path='/' element={<Home />}/>
+            <Route path='/posts' element={<PostList />}/>
+            <Route path='/posts/:id' element={<PostDetail />}/>
+            <Route path='/posts/new' element={<PostNew />}/>
+            <Route path='/posts/edit/:id' element={<PostEdit />}/>
+            <Route path='/profile' element={<ProfilePage />}/>
+            <Route path='/login' element={<LoginPage />}/>
+            <Route path='/signup' element={<SignupPage />}/>
+            {/* 잘못된 경로 진입시 돌아갈 경로 설정 */}
+            <Route path='*' element={<Navigate replace to="/" />} />  
+          </>
+        ) : (
+          <>
+            <Route path='/login' element={<LoginPage />}/>
+            <Route path='/signup' element={<SignupPage />}/>
+            <Route path='*' element={<LoginPage />}/>
+          </>
+        )
+      }
       </Routes>
     </>
   );
