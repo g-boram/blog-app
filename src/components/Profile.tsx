b70e106e7602af4ae718fc7710209eb6343bc495
GOBORAM <109337202+g-boram@users.noreply.github.com>
2023-12-29 17:28:18 +0900

    #3-context API로 사용자 관리

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,31 +1,35 @@
+import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
+import { useContext } from "react";
 import { toast } from "react-toastify";
 
-export default function Profile() {
-  const auth = getAuth(app);
-
-  const onSignOut = async () => {
-    try {
-      const auth = getAuth(app);
-      await signOut(auth);
-      toast.success("로그아웃 되었습니다.");
-    } catch(error: any) {
-      console.log(error);
-      toast.error(error?.code);
-    }
+// 로그아웃
+const onSignOut = async () => {
+  try {
+    const auth = getAuth(app);
+    await signOut(auth);
+    toast.success("로그아웃 되었습니다.");
+  } catch(error: any) {
+    console.log(error);
+    toast.error(error?.code);
   }
-  
+}
+
+export default function Profile() {
+  const { user } = useContext(AuthContext);
+
+
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image" />
         <div>
           <div className="profile__email">
-            {auth?.currentUser?.email}
+            {user?.email}
           </div>
           <div className="profile__name">
-            {auth?.currentUser?.displayName || "User"}
+            {user?.displayName || "User"}
           </div>
         </div>
       </div>
